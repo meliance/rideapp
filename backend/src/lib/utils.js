fcebde67dff1ab3ce4d0e92c46f1f6ac5f847b0d
meliance <melakmu66@gmail.com>
2026-08-26 03:18:51 +0300

    fixed

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = (userId, res) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+export const generateToken = (id, role, res) => {
+    const token = jwt.sign({ id, role }, process.env.JWT_SECRET, {
         expiresIn: "7d"
     });
     
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
-        httpOnly: true, // Prevents XSS attacks
-        sameSite: "strict", // Prevents CSRF attacks
+        httpOnly: true, 
+        sameSite: "strict",
         secure: process.env.NODE_ENV !== "development"
     });
     
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { signup, login, logout, checkAuth } from "../controllers/auth.controller.js";
+import { 
+  signup, 
+  driverSignup, 
+  login, 
+  logout, 
+  checkAuth 
+} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/driver/signup", driverSignup);
 router.post("/login", login);
 router.post("/logout", logout);
 
